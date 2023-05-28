9ee790a3d2a1cacca407e769e01619a1beeb501f
thezulux24 <111769151+thezulux24@users.noreply.github.com>
2023-05-27 22:24:33 -0500

    Update big.h

--- proyecto/big.h
+++ proyecto/big.h
@@ -1,35 +1,40 @@
 
 #include <vector>
 #include <string>
+#include <list>
 
 #ifndef BIG_H
 #define BIG_H
 
 using namespace std;
-typedef string Elemento;
 
 class BigInteger{
     vector<int> vec;
-  
+    bool negativo;
     public:
-        BigInteger(Elemento e);
-        BigInteger(const BigInteger &big);
-        void add(BigInteger big);
-        void product(BigInteger big);
-        void substract(BigInteger big);
-        void quotient(BigInteger big);
-        void remainder(BigInteger big);
-        void pow(BigInteger big);
+        BigInteger();
+        BigInteger(const string e);
+        BigInteger(const BigInteger& big);
+        void add(BigInteger& big);
+        void product(BigInteger& big);
+        void substract(BigInteger& big);
+        void quotient(BigInteger& big);
+        void remainder(BigInteger& big);
+        void pow(int e);
         string toString();
         //sobrecarga
-        BigInteger operator+(BigInteger big);
-        BigInteger operator-(BigInteger big);
-        BigInteger operator*(BigInteger big);
-        BigInteger operator/(BigInteger big);
-        BigInteger operator%(BigInteger big);
-        bool operator==(const BigInteger& big) const;
-        bool operator<(const BigInteger& big) const;
-        bool operator<=(const BigInteger& big) const;
+        BigInteger operator+(BigInteger& big);
+        BigInteger operator-(BigInteger& big);
+        BigInteger operator*(BigInteger& big);
+        BigInteger operator/(BigInteger& big);
+        BigInteger operator%(BigInteger& big);
+        bool operator==( BigInteger& big) ;
+        bool operator<(BigInteger& big) ;
+        bool operator<=( BigInteger& big);
+
+        //estaticas
+        static BigInteger sumarListaValores(list<BigInteger>& lista);
+        static BigInteger multiplicarListaValores(list<BigInteger>& lista);
 };
 
 #endif
